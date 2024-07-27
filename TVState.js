f30c7036e048f7671e61335906343e73d0f013e6
Dustinlee39 <98402471+Dustinlee39@users.noreply.github.com>
2024-07-27 11:26:20 -0500

    Create TVState.js

---
+++ TVState.js
@@ -0,0 +1,63 @@
+class TVState {
+    constructor() {
+        if (TVState.instance) {
+            return TVState.instance;
+        }
+        this.powerOn = false;
+        this.currentInputIndex = 0;
+        this.volume = 50;
+        this.speed = 1;
+        this.observers = [];
+        TVState.instance = this;
+    }
+
+    addObserver(observer) {
+        this.observers.push(observer);
+    }
+
+    notifyObservers() {
+        this.observers.forEach(observer => observer.update(this));
+    }
+
+    togglePower() {
+        this.powerOn = !this.powerOn;
+        this.notifyObservers();
+    }
+
+    changeInput() {
+        if (this.powerOn) {
+            this.currentInputIndex = (this.currentInputIndex + 1) % 4; // Assuming 4 inputs
+            this.notifyObservers();
+        } else {
+            alert('Power is off. Please turn on the power first.');
+        }
+    }
+
+    volumeUp() {
+        if (this.volume < 100) {
+            this.volume += 10;
+            this.notifyObservers();
+        }
+    }
+
+    volumeDown() {
+        if (this.volume > 0) {
+            this.volume -= 10;
+            this.notifyObservers();
+        }
+    }
+
+    speedUp() {
+        if (this.speed < 2) {
+            this.speed += 0.1;
+            this.speed = parseFloat(this.speed.toFixed(1));
+            this.notifyObservers();
+        }
+    }
+
+    speedDown() {
+        if (this.speed > 0.5) {
+            this.speed -= 0.1;
+            this.speed = parseFloat(this.speed.toFixed(1));
+            this.notifyObservers();
+        }
